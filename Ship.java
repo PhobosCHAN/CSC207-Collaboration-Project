795f32f40e47a0bd85e9eb8364001ee7d3bea235
aryamansuri <arysuri21@gmail.com>
2022-11-23 18:04:13 -0500

    Initial commit

---
+++ Ship.java
@@ -0,0 +1,19 @@
+
+public class Ship {
+    String type;
+    int size;
+    int hp;
+
+    public Ship(String type, int size) {
+        this.type = type;
+        this.size = size;
+    }
+
+    public int getSize(){
+        return this.size;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+}
