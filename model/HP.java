9fe891a23fbe5e90db662ca5892442e9b67922f7
aryamansuri <arysuri21@gmail.com>
2022-11-29 19:10:55 -0500

    Couple changes to the implementation of HP and the use of Singleton design pattern.

--- model/HP.java
+++ model/HP.java
@@ -3,8 +3,14 @@ package model;
 public class HP {
 
     private static HP instance = null;
-    public int hp_large = 20;
-    public int hp_small = 10;
+    /**
+     * Starting player HP for the normal mode of Battle Ship where there are ships of sizes: 5, 4, 3, 3, 2
+     */
+    public int hp_large = 17;
+    /**
+     * Starting player HP for the fast mode of Battle Ship where there are ships of sizes: 3, 3, 2
+     */
+    public int hp_small = 8;
     private HP() {}
 
     public static synchronized HP getInstance(){
