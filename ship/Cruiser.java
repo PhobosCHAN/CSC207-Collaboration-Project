f154a6ca46d55efca6f5f52de1d5570eb1715dd7
aryamansuri <arysuri21@gmail.com>
2022-12-05 17:05:10 -0500

    Added JavaDoc to Ship package

--- ship/Cruiser.java
+++ ship/Cruiser.java
@@ -2,38 +2,70 @@ package ship;
 
 import model.BattlePoint;
 
-/** An cruiser ship.
+/** A cruiser ship.
  */
 public class Cruiser implements Ship {
+
+    /**
+     * Cruiser Ship.
+     * A ship with length 4.
+     */
     BattlePoint[] body;
     public boolean vertical = true;
 
     private int health;
+
+    /**
+     * Constructor for a Cruiser Ship.
+     *
+     * @param body    a list of BattlePoints.
+     * @param vertical    boolean value for the orientation of the ship.
+     */
     public Cruiser(BattlePoint[] body, boolean vertical){
         this.body = body;
         this.health = 4;
         this.vertical = vertical;
     }
+
+    /**
+     * Getter for ship HP
+     */
     public int getHP(){
         return this.health;
     }
 
-    @Override
+    /**
+     * Getter for ship Body
+     */
     public BattlePoint[] getBody() {
         return this.body;
     }
 
+    /**
+     * Deducts the ship health by 1 since it's been hit.
+     */
     public void hit() {
         health--;
     }
 
+    /**
+     * Returns true if the ship's health is more than 0 and the ship is alive.
+     */
     public boolean isAlive() {
         return health > 0;
     }
 
+    /**
+     * Getter for ship Vertical
+     */
     public boolean getVertical(){
         return this.vertical;
     }
+
+    /**
+     * Setter for ship's body based on its orientation(vertical or horizontal) and
+     * the placement of the origin of the ship on the board(given by x and y).
+     */
     public void setBody(boolean vert, int x, int y) {
         body = new BattlePoint[4];
         for(int i = 0; i < 4; i++){
@@ -50,6 +82,9 @@ public class Cruiser implements Ship {
         }
     }
 
+    /**
+     * Setter for ship Vertical
+     */
     @Override
     public void setVertical(boolean vert) {
         this.vertical = vert;
